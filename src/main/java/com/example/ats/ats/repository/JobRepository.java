fe41e6e72479d25dfc1f2a232a3fcbc0b5a6afc9
lavanyamiryala47 <lavanyamiryala47@gmail.com>
2025-08-16 15:06:32 +0530

    Fix package structure and add render.yaml

--- src/main/java/com/example/ats/ats/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.example.ats.repository;
-
-import com.example.ats.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
-
